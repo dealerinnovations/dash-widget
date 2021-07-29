1c4f6ebe45370100f6f695571604784f51c23997
Anh <doananh234@gmail.com>
2021-07-30 00:47:38 +0700

    feat: add add zindex for close

--- src/index.js
+++ src/index.js
@@ -76,6 +76,7 @@ const injectDashWidget = dealerConfigs => {
       closeButton.style.alignItems = 'center';
       closeButton.style.justifyContent = 'center';
       closeButton.style.padding = '15px';
+      closeButton.style.zIndex = '1000';
 
       closeButton.onclick = () => {
         document.getElementById('dash--iframe-wrapper').remove();
