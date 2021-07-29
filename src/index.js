13ba6ea53e6356721a04d12692a6d5c126d5abab
Anh <doananh234@gmail.com>
2021-07-29 09:23:59 +0700

    feat: change redirect to iframe

--- src/index.js
+++ src/index.js
@@ -20,14 +20,72 @@ const injectDashWidget = dealerConfigs => {
     dealerConfigs.customer_data &&
     dealerConfigs.customer_data.widget &&
     dealerConfigs.customer_data.widget.button;
+
   for (let i = 0; i < vehicles.length; i += 1) {
     const vin = vehicles[i].getAttribute('data-dash-vehicle-vin');
-    const node = document.createElement('a');
+    const node = document.createElement('div');
     node.innerHTML = button || DEFAULT_DASH_BUTTON_STYLE;
-    node.href =
-      dealerConfigs.domain && dealerConfigs.domain.indexOf('http') > -1
-        ? `${dealerConfigs.domain}/vehicle/${vin}`
-        : `https://${dealerConfigs.domain}/vehicle/${vin}`;
+    node.onclick = () => {
+      const iframe = document.createElement('iframe');
+      iframe.src =
+        dealerConfigs.domain && dealerConfigs.domain.indexOf('http') > -1
+          ? `${dealerConfigs.domain}/vehicle/${vin}`
+          : `https://${dealerConfigs.domain}/vehicle/${vin}?iframe=${window.location.pathname}`;
+      iframe.style.position = 'fixed';
+      iframe.style.top = '0px';
+      iframe.style.bottom = '0px';
+      iframe.style.left = '0px';
+      iframe.style.right = '0px';
+      iframe.width = '100%';
+      iframe.height = '100%';
+      iframe.style.border = 'none';
+
+      const elmDiv = document.createElement('div');
+      elmDiv.id = 'dash--iframe-wrapper';
+      elmDiv.style.position = 'fixed';
+      elmDiv.style.top = '0px';
+      elmDiv.style.bottom = '0px';
+      elmDiv.style.left = '0px';
+      elmDiv.style.right = '0px';
+      elmDiv.style.backgroundColor = 'white';
+      elmDiv.style.width = '100vw';
+      elmDiv.style.height = '100vh';
+      elmDiv.style.display = 'flex';
+      const loadingImg = document.createElement('img');
+      loadingImg.src = 'http://media.giphy.com/media/s4KqhlPU9Ypnq/giphy.gif';
+      loadingImg.style.width = '100px';
+      loadingImg.style.height = '100px';
+      loadingImg.style.objectFit = 'contain';
+      loadingImg.style.margin = 'auto';
+      elmDiv.appendChild(loadingImg);
+
+      elmDiv.appendChild(iframe);
+
+      const closeButton = document.createElement('div');
+      closeButton.style.position = 'fixed';
+      closeButton.style.top = '20px';
+      closeButton.style.right = '20px';
+      closeButton.style.backgroundColor = '#000';
+      closeButton.style.width = '50px';
+      closeButton.style.height = '50px';
+      closeButton.style.color = '#fff';
+      closeButton.style.borderRadius = '25px';
+      closeButton.textContent = 'X';
+      closeButton.style.display = 'flex';
+      closeButton.style.alignItems = 'center';
+      closeButton.style.justifyContent = 'center';
+      closeButton.style.padding = '15px';
+
+      closeButton.onclick = () => {
+        document.getElementById('dash--iframe-wrapper').remove();
+      };
+
+      elmDiv.appendChild(closeButton);
+      document.body.appendChild(elmDiv);
+    };
+    // dealerConfigs.domain && dealerConfigs.domain.indexOf('http') > -1
+    //   ? `${dealerConfigs.domain}/vehicle/${vin}`
+    //   : `https://${dealerConfigs.domain}/vehicle/${vin}`;
     node.style.textDecoration = 'auto';
     vehicles[i].appendChild(node);
   }
